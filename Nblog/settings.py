a8d3c702bd3e0649b52c79e01267e71ccaaf4fc9
athenavi <support@7trees.cn>
2025-05-31 23:12:10 +0800

    func: Upload dependency file

--- Nblog/settings.py
+++ Nblog/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-zm*oao=zn53km8jlw=%ava7(ewrc=6+#whs4xg@@nw$37dux1^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
